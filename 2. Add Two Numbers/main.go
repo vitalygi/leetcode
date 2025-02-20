cbd15f98e2c49657109c692bd2b1fabd13d765c8
aruvit <vitalijarutunan@MacBook-Air-Vitalij.local>
2025-02-20 23:16:03 +0300

    add two numbers

---
+++ 2. Add Two Numbers/main.go
@@ -0,0 +1,31 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	answerNode := &ListNode{}
+	curr := answerNode
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carry = sum / 10
+		curr.Next = &ListNode{Val: sum % 10}
+		curr = curr.Next
+	}
+
+	return answerNode.Next
+}
